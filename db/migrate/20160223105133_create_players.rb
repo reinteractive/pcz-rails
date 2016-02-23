9951296416ba2495eb48f329cfb88a20fa2a11ac
Kapya <kapsakala@gmail.com>
2016-02-23 14:17:11 +0200

    Created players table with fideid as primary key

---
+++ db/migrate/20160223105133_create_players.rb
@@ -0,0 +1,18 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players, {:id =>false} do |t|
+      t.integer :fideid
+      t.string :name
+      t.string :sex
+      t.string :title
+      t.integer :rating
+      t.integer :k
+      t.integer :rapid
+      t.integer :blitz
+      t.integer :birthday
+      t.string :flag
+
+      t.timestamps null: false
+    end
+  end
+end
