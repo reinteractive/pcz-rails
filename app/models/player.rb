8fd411f27f9387141dabdec66ae52f1beb6e0867
Kapya <kapsakala@gmail.com>
2016-02-24 12:52:15 +0200

    Now able to import players list from csv file

---
+++ app/models/player.rb
@@ -0,0 +1,18 @@
+class Player < ActiveRecord::Base
+	require 'csv'
+
+	def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+
+      player_hash = row.to_hash
+      player = Player.where(fideid: player_hash["fideid"])
+
+      if player.count == 1
+        player.first.update_attributes(player_hash)
+      else
+        Player.create!(player_hash)
+      end # end if !product.nil?
+    end # end CSV.foreach
+  end # end self.import(file)
+
+end
