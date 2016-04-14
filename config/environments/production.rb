a2152ed0675495cc4c6d7b12643127e6eee04a30
Kapya <kapsakala@gmail.com>
2016-04-14 22:01:12 +0200

    paperclip image config nonsense

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,8 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-  :url =>':s3_domain_url'
+  :url =>':s3_domain_url',
+  :path = '/:class/:attachment/:id_partition/:style/:filename'
 }
 
   # Prepend all log lines with the following tags.
