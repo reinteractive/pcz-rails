559d5ae8cbd3edeb677a6f7aae337d2445b8da1f
Kapya <kapsakala@gmail.com>
2016-04-14 21:56:02 +0200

    paperclip image upload nearly complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	validates :image, presence: true
 	validates_uniqueness_of :title
 
-	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }
+	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }, :storage => :s3, :url => ':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,7 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-  :url =>':s3_domain_url',
-  :path => '/:class/:attachment/:id_partition/:style/:filename'
+  :url =>':s3_domain_url'
 }
 
   # Prepend all log lines with the following tags.
