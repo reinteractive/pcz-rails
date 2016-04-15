1414ae6dd34a81265f6780fd9dfb61ac55cb940c
Kapya <kapsakala@gmail.com>
2016-04-15 02:02:53 +0200

    image upload to s3(please work this time)

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	validates :image, presence: true
 	validates_uniqueness_of :title
 
-	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }, :storage => :s3, :url => ':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename',:s3_host_name => 's3-us-west-2.amazonaws.com',:bucket => 'pcz-assets'
+	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => ":attachment/:id/:style.:extension"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => 'us-east-1',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
@@ -52,4 +53,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
