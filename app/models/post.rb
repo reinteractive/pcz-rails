cc8600dccc28f0d7369b9e3e43e18843cd28d6bb
Kapya <kapsakala@gmail.com>
2016-04-15 02:17:43 +0200

    corrected rails root

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	validates :image, presence: true
 	validates_uniqueness_of :title
 
-	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => ":attachment/:id/:style.:extension"
+	has_attached_file :image, styles: { :small => "350x", :mini => "200x" }, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :path => ":attachment/:id/:style.:extension"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
