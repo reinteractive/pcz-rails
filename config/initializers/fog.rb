ecbc0b24645254a8c554cb637e7ea89ee0c17ceb
Kapya <kapsakala@gmail.com>
2016-04-17 06:03:48 +0200

    minor correction(again)

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
 
     # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                => 'us-west-2'
+    provider              : 'AWS',
+    aws_access_key_id     : ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key : ENV['AWS_SECRET_ACCESS_KEY'],
+    region                : 'us-west-2'
   }
 
   # For testing, upload files to local `tmp` folder.
