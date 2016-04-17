f6567c27d8e6241a321e8acb1a529c12a7054a32
Kapya <kapsakala@gmail.com>
2016-04-17 06:27:09 +0200

    fog nonsense errors

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,7 +1,7 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  #config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     # Configuration for Amazon S3 should be made available through an Environment variable.
     # For local installations, export the env variable through the shell OR
