a96c84d1af2d28db7c65b9a685e89718d82fe7d3
Kapya <kapsakala@gmail.com>
2016-04-17 06:35:43 +0200

    fog nonsense errors

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -15,7 +15,6 @@ CarrierWave.configure do |config|
     provider:'AWS',
     aws_access_key_id:ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key:ENV['AWS_SECRET_ACCESS_KEY'],
-    region:'us-west-2'
   }
 
   # For testing, upload files to local `tmp` folder.
